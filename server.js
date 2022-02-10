ee1cd17b7dd06a84dc8474c33690115e291fd28b
vvaytalov <vvaytalov@dtln.ru>
2022-02-10 15:50:59 +0300

    правка по комментариям

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000;
+const port = process.env.PORT || PORT;
 
 app.use(express.static(`${__dirname}/dist`));
 
-app.listen(PORT, function () {
-    console.log(`Example app listening on port ${PORT}!`);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
 }); 
